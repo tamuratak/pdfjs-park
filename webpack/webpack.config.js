96d357e111e42db0635ec333a309a36690186fad
Takashi Tamura <tamuratak@users.noreply.github.com>
2021-07-11 10:47:25 +0900

    Add webpack/

---
+++ webpack/webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require("webpack"); // eslint-disable-line no-unused-vars
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    main: "./main.js",
+    "pdf.worker": "pdfjs-dist/build/pdf.worker.entry",
+  },
+  mode: "none",
+  output: {
+    path: path.join(__dirname, "build"),
+    publicPath: "./build",
+    filename: "[name].bundle.js",
+  },
+  devServer: {
+    contentBase: __dirname,
+    compress: true,
+    historyApiFallback: true, // respond to 404s with index.html
+    host: 'localhost',
+    hot: true, // enable HMR on the server
+    port: 3000,
+  },
+};
