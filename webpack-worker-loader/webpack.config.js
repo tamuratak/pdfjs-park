a0903b42aff58bc0aacd965e6574565fd2da336f
Takashi Tamura <tamuratak@users.noreply.github.com>
2021-07-11 20:44:23 +0900

    Add webpack-worker-loader

---
+++ webpack-worker-loader/webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require("webpack"); // eslint-disable-line no-unused-vars
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    main: "./main.js"
+  },
+  mode: "none",
+  output: {
+    path: path.join(__dirname, "build"),
+    publicPath: "./build/",
+    filename: "[name].bundle.js",
+  },
+  devServer: {
+    contentBase: __dirname,
+    compress: true,
+    historyApiFallback: true, // respond to 404s with index.html
+    host: 'localhost',
+    hot: true, // enable HMR on the server
+    port: 3000,
+  },
+};
