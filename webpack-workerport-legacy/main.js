5ead52622eea3413ccdb6649977ede0b78b98225
Takashi Tamura <tamuratak@users.noreply.github.com>
2023-11-23 11:09:16 +0900

    edit

--- webpack-workerport-legacy/main.js
+++ webpack-workerport-legacy/main.js
@@ -7,7 +7,10 @@ import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf.mjs';
 
 const pdfPath = "/abc.pdf";
 
-const worker = new Worker(new URL('./node_modules/pdfjs-dist/legacy/build/pdf.worker.mjs', import.meta.url), { type: 'module' });
+const worker = new Worker(
+  new URL('./node_modules/pdfjs-dist/legacy/build/pdf.worker.mjs', import.meta.url),
+  { type: 'module' }
+);
 // Setting worker path to worker bundle.
 pdfjsLib.GlobalWorkerOptions.workerPort = worker;
 
